1a7f6d55f8bd646a596e8200478d473506e21d64
Haojerh <haojerh@gmail.com>
2025-11-24 22:57:59 +0800

    Added component folder

---
+++ frontend/component/footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Outfit:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Sixtyfour&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../style/global.css">
+    <link rel="stylesheet" href="../style/component.css">
+</head>
+<body>
+    <div class="col-12 col-s-12 footer fade-in">
+        <span class="footer-text">© 2025 APLITE - Asia Pacific University. Building a sustainable future together.</span>
+        <div>
+            <a href=""><img src="../image/facebook.png" alt="Facebook" class="social-button"/></a>
+            <a href=""><img src="../image/instagram.png" alt="Instagram" class="social-button"/></a>
+        </div>
+    </div>
+</body>
+</html>
