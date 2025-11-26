9e81dd7d48863a6bdd8748f5a4844734673a9d88
Sia Yu <ongsiayu18@gmail.com>
2025-11-27 00:08:05 +0800

    login layout kinda done

--- frontend/pages/login/login.php
+++ frontend/pages/login/login.php
@@ -3,34 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Outfit:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Sixtyfour&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Outfit:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Sixtyfour&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>APLite Login</title>
     <link rel="stylesheet" href="../../styles/login.css">
     <link rel="stylesheet" href="../../styles/global.css">
 </head>
 <body>
     <div class="wrapper">
-        <div class="title-intro">
-            <span class="big-heading">Hello, Welcome Back</span>
-            <span class="small-heading">We are happy to have you back in APLite</span>
+        <div class="left-contents">
+            <div class="title-intro">
+                <span class="big-heading">Hello, Welcome Back</span>
+                <span class="small-heading">We are happy to have you back in APLite</span>
+            </div>
+            <form action="#">
+                <div class="input-area">
+                    <div class="input-box">
+                        <input type="text" placeholder="enter username" required>
+                    </div>
+
+                    <div class="input-box">
+                        <input type="password" placeholder="enter password" required>
+                    </div>
+
+                    <div class="remember">
+                        <input type="checkbox">
+                        <span>Remember Me</span>
+                    </div>
+                </div>
+                <div><button type="submit"><span class="gradient-text">Sign In</span></button></div>
+            </form>
         </div>
-        <form action="#">
-            <div class="input-area">
-                <div class="input-box">
-                    <input type="text" placeholder="Username" required>
-                </div>
-                <div class="input-box">
-                    <input type="password" placeholder="Password" required>
-                </div>
-            </div>
-
-            <div class="remember">
-                <input type="checkbox">
-                <label>Remember Me</label>
-            </div>
-
-            <button type="submit">Sign In</button>
-        </form>
+        <div class="right-contents">
+        </div>
     </div>
 </body>
 </html>
