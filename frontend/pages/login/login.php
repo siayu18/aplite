93d83706ec65c7b78f47456612b80b3cedff7d88
Sia Yu <ongsiayu18@gmail.com>
2025-12-03 23:49:26 +0800

    completed scammer login page

--- frontend/pages/login/login.php
+++ frontend/pages/login/login.php
@@ -5,15 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Outfit:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Sixtyfour&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>APLite Login</title>
-    <!-- <link rel="stylesheet" href="../../styles/login.css"> -->
     <link rel="stylesheet" href="../../styles/global.css">
-    <link rel="stylesheet" href="../../styles/login2.css">
+    <link rel="stylesheet" href="../../styles/login.css">
 </head>
 <body>
     <div class="wrapper">
         <div class="container">
             <div class="title-intro">
-                <span class="big-heading">Hello, Welcome Back!!!</span>
+                <span class="big-heading">Hello, Welcome Back</span>
                 <span class="small-heading">We are happy to have you back in APLite</span>
             </div>
             <form action="#">
@@ -34,8 +33,11 @@
                 <div><button type="submit"><span class="gradient-text">Sign In</span></button></div>
             </form>
         </div>
-        <div class="container">
-            <div class="right-contents"></div>
+        <div class="login-bg">
+            <div class="bg-text">
+                <h2>Building a sustainable future</h2>
+                <p>saving energy with a few clicks</p>
+            </div>
         </div>
     </div>
 </body>
