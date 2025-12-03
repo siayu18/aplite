3090705d69aacea4a2701b384d3d21f0b7d83c55
Sia Yu <ongsiayu18@gmail.com>
2025-12-03 22:11:41 +0800

    Learning css for login

--- frontend/pages/login/login.php
+++ frontend/pages/login/login.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Outfit:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Sixtyfour&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>APLite Login</title>
-    <link rel="stylesheet" href="../../styles/login.css">
+    <!-- <link rel="stylesheet" href="../../styles/login.css"> -->
     <link rel="stylesheet" href="../../styles/global.css">
+    <link rel="stylesheet" href="../../styles/login2.css">
 </head>
 <body>
     <div class="wrapper">
-        <div class="left-contents">
+        <div class="container">
             <div class="title-intro">
-                <span class="big-heading">Hello, Welcome Back</span>
+                <span class="big-heading">Hello, Welcome Back!!!</span>
                 <span class="small-heading">We are happy to have you back in APLite</span>
             </div>
             <form action="#">
@@ -33,7 +34,8 @@
                 <div><button type="submit"><span class="gradient-text">Sign In</span></button></div>
             </form>
         </div>
-        <div class="right-contents">
+        <div class="container">
+            <div class="right-contents"></div>
         </div>
     </div>
 </body>
