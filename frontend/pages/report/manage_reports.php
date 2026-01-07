e686dc877bdf2af427a0cfb128e91c10a7d48d8e
Owner <darrenw2006@gmail.com>
2026-01-07 22:14:09 +0800

    fitler -> filter

--- frontend/pages/report/manage_reports.php
+++ frontend/pages/report/manage_reports.php
@@ -93,7 +93,7 @@ mysqli_close($con);
                 </div>
 
                 <div class="label-field">
-                    <label class="green-description">Fitler By Status</label>
+                    <label class="green-description">Filter By Status</label>
                     <select class="dropdown-classroom-choice" name="status">
                         <option value="all">All Status</option>
                         <option value="approved">Approved</option>
